3829600a96d7a73e5564d433eb77d44b9d2bfc9e
RomanOmelianenko <r_omelianenko@ukr.net>
2020-10-04 21:43:12 +0300

    add-menu-ingredients

--- src/js/templating.js
+++ src/js/templating.js
@@ -1,6 +1,8 @@
 import itemsTemplate from '../templates/gallery-items.hbs';
 import menu from '../menu.json';
 
-const markup = itemsTemplate(menu);
+// const markup = itemsTemplate(menu);
 const galleryMenuRef = document.querySelector('.js-menu');
+const markup = menu.map(el => itemsTemplate(el)).join('');
 galleryMenuRef.insertAdjacentHTML('beforeend', markup);
+// galleryMenuRef.insertAdjacentHTML('beforeend', markup);
